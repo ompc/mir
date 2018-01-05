8f4bc82be2a19071d7f378227f0fdbc28362510d
dukun <dukun@alibaba-inc.com>
2018-01-05 19:34:26 +0800

    update

---
+++ bin/gen-daily-md.sh
@@ -0,0 +1,42 @@
+# program : gen-detail-md.sh
+#  author : oldmanpushcart@gmail.com
+#    date : 2018-01-03
+# version : 0.0.1
+#    desc : 通过当日CSV流水生成当日MD详情页
+#!/usr/bin/env bash
+
+# MD文件输出路径
+DETAIL_MD_OUT_DIR="../out/detail"
+
+# 指定需要生成的账务日期${1},yyyy-mm-dd
+TARGET_DATE=${1}
+[ -z ${TARGET_DATE} ]&&TARGET_DATE=$(date +%Y-%m-%d)
+
+# 源CSV文件
+SOURCE_CSV_FILE="../data/${TARGET_DATE%-*}/${TARGET_DATE}/${TARGET_DATE}.csv"
+[ ! -f ${SOURCE_CSV_FILE} ]&&exit
+
+
+# 目标MD文件
+DETAIL_MD_FILE="${DETAIL_MD_OUT_DIR}/${TARGET_DATE}.md"
+
+
+## 生成页头
+printf "## 当日积分排行榜：${TARGET_DATE}\n" > ${DETAIL_MD_FILE}
+
+## 生成明细下载链接
+printf "[积分流水明细下载](../../data/${TARGET_DATE%-*}/${TARGET_DATE}/${TARGET_DATE}.csv)\n" >> ${DETAIL_MD_FILE}
+
+## 生成排行榜
+echo -n "
+排名|游戏玩家|积分|场次
+:---:|---|---|---
+" >> ${DETAIL_MD_FILE}
+
+cat ${SOURCE_CSV_FILE} \
+    | awk -F "," '{m[$2]+=$4;c[$2]++;}END{for(i in m)printf("%s,%0.2f,%s\n",i,m[i],c[i])}' \
+    | sort -t ',' -nrk2,2 \
+    | awk -F "," '{if(NR<=3){printf("![](https://raw.githubusercontent.com/ompc/mir/master/out/img/TOP%s.png)|**%s**|**%s**|**%s**\n",NR,$1,$2,$3)}else{printf("第%s名|%s|%s|%s\n",NR,$1,$2,$3)}}' \
+    >> ${DETAIL_MD_FILE}
+
+echo "${DETAIL_MD_FILE} was generated."
--- bin/gen-daily.sh
+++ bin/gen-daily.sh
@@ -130,31 +130,14 @@ rm -f ${TARGET_DATE_CSV_FILE} \
         >> ${_tmp_TARGET_DATE_CSV_FILE}
     done
 
-# 生成MD文件
-function gen_detail_md
-{
-    mkdir -p ../out/detail
-    _md_file="../out/detail/${TARGET_DATE}.md"
-
-echo -n "
-## ${TARGET_DATE}-积分变动明细
-
-操作日期|游戏玩家|类型|数值|变更缘由
----|---|---|---|---
-" > ${_md_file}
-
-    cat ${TARGET_DATE_CSV_FILE} \
-        | grep -vE "^$" \
-        | sort -t ',' -k2,2 \
-        | awk -F "," '{printf("%s|%s|%s|%s|%s\n",$1,$2,$3,$4,$5)}' \
-        >> ${_md_file}
-}
-
 # 正式替换CSV的临时文件为正式CSV文件
 if [[ -f ${_tmp_TARGET_DATE_CSV_FILE} ]]; then
     mv ${_tmp_TARGET_DATE_CSV_FILE} ${TARGET_DATE_CSV_FILE}
     echo "${TARGET_DATE_CSV_FILE} was generated."
-    gen_detail_md
+
+    # 生成MD
+    ./gen-daily-md.sh ${TARGET_DATE}
+
 else
     echo "${TARGET_DATE_CSV_FILE} gen failed."
 fi
