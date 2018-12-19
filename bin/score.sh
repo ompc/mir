725b7b0ed8c639fdb4f1e604935bf564277164b5
vlinux <oldmanpushcart@gmail.com>
2018-12-20 03:01:31 +0800

    backup for 2018-12-20

--- bin/score.sh
+++ bin/score.sh
@@ -17,5 +17,5 @@ while read line;do
 	tt_player="tt"$(echo ${line}|awk '{print $1}')
 	score=$(echo ${line}|awk '{print $2}')
 	token=$(head -1000 /dev/urandom|md5sum|awk '{print $1}')
-	curl "http://127.0.0.1:8080/guild/${gid}/player/${tt_player}/score/change/admin?admin=${tt_admin}&score=${score}&token=${token}"
+	curl "http://127.0.0.1:8080/guild/${gid}/player/${tt_player}/score/change/admin?admin=tt${tt_admin}&score=${score}&token=${token}"
 done
