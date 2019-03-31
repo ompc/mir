e8909cc4ccebeeffdfe8ca3a963a7df1a2a103c3
vlinux <oldmanpushcart@gmail.com>
2019-04-01 03:02:16 +0800

    backup for 2019-04-01

---
+++ bin/clean-score.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl 'http://127.0.0.1:8080/admin/clean/guild/1003?owner=tt2024288'
+curl 'http://127.0.0.1:8080/admin/clean/guild/1006?owner=tt11343935'
+curl 'http://127.0.0.1:8080/admin/clean/guild/1038?owner=tt19903829'
+curl 'http://127.0.0.1:8080/admin/clean/guild/1047?owner=tt89443171'
+curl 'http://127.0.0.1:8080/admin/clean/guild/1051?owner=tt95227924'
+curl 'http://127.0.0.1:8080/admin/clean/guild/1053?owner=tt7930986'
